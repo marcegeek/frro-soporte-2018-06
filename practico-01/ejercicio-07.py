b0ea665e3626d99b6835da116a9b06a5ffb62d85
Marcelo Castellano <marce.geek22@gmail.com>
2018-04-03 22:06:27 -0300

    agrego tp 1 ejercicio 7

---
+++ practico-01/ejercicio-07.py
@@ -0,0 +1,15 @@
+# Definir una función es_palindromo() que reconoce palíndromos
+
+
+def es_palindromo(str):
+    for i in range(0, len(str) // 2):
+        if str[i] != str[len(str) - i - 1]:
+            return False
+    return True
+
+
+assert es_palindromo('radar'), es_palindromo('radar')
+assert es_palindromo('reconocer'), es_palindromo('reconocer')
+assert not es_palindromo('banana'), not es_palindromo('banana')
+assert es_palindromo('a'), es_palindromo('a')
+assert es_palindromo('😀'), es_palindromo('😀')
