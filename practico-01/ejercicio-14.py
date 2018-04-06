83987b550594bd648132d3940f71551865968c7c
Marcelo Castellano <marce.geek22@gmail.com>
2018-04-06 13:35:48 -0300

    agrego descripción del problema del laberinto

---
+++ practico-01/ejercicio-14.py
@@ -0,0 +1,5 @@
+# Búsqueda de salida en un laberinto
+# El laberinto se codifica como una matriz booleana de m filas por n columnas
+# Las celdas con valor True son obstáculos mientras que las celdas con valor
+# False son celdas por las que se puede caminar
+# Hay un único punto de entrada al laberinto y otro único de salida.
