886ed2ebf6a799ba25b35545329a322b31ecf247
Marcelo Castellano <marce.geek22@gmail.com>
2018-04-08 20:20:35 -0300

    agrego detalles de codificación al problema del laberinto

--- practico-01/ejercicio-14.py
+++ practico-01/ejercicio-14.py
@@ -3,3 +3,8 @@
 # Las celdas con valor True son obstáculos mientras que las celdas con valor
 # False son celdas por las que se puede caminar
 # Hay un único punto de entrada al laberinto y otro único de salida.
+
+# La matriz del laberinto es una lista de listas con valores booleanos:
+# lab[i][j] es el valor de la celda en la fila i, columna j
+# Los puntos de entrada y salida se codifican como tuplas de 2 elementos:
+# (i, j) representa la posición de la celda correspondiente
