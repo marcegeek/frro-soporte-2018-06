8e9faefc012ed64358ae152e221297b62e9a6315
Marcelo Castellano <marce.geek22@gmail.com>
2018-04-10 18:11:55 -0300

    agrego búsqueda de salida del laberinto

--- practico-01/ejercicio-14.py
+++ practico-01/ejercicio-14.py
@@ -29,27 +29,26 @@ def validar_laberinto(lab, entrada, salida):
     return True, ''
 
 
-def transformar_laberinto(lab, entrada, salida):
+def laberinto_mostrable(lab, entrada, salida):
     val, err = validar_laberinto(lab, entrada, salida)
     if not val:
         raise ValueError(err)
-    matriz = []
+    mostrable = []
     for fila in lab:
-        matriz.append([])
+        mostrable.append([])
         for col in fila:
             if col:
-                matriz[-1].append(0)
+                mostrable[-1].append(0)
             else:
-                matriz[-1].append(1)
-    matriz[entrada[0]][entrada[1]] = 2
-    matriz[salida[0]][salida[1]] = 3
-    return matriz
+                mostrable[-1].append(1)
+    mostrable[entrada[0]][entrada[1]] = 2
+    mostrable[salida[0]][salida[1]] = 3
+    return mostrable
 
 
-def mostrar_laberinto(lab, entrada, salida):
-    matriz = transformar_laberinto(lab, entrada, salida)
-    filas = len(matriz)
-    columnas = len(matriz[0])
+def mostrar_laberinto(mostrable, entrada, salida):
+    filas = len(mostrable)
+    columnas = len(mostrable[0])
     print('┌', end='')
     for j in range(columnas):
         if (0, j) not in (entrada, salida):
@@ -66,7 +65,7 @@ def mostrar_laberinto(lab, entrada, salida):
             dicc = {0: 'x', 1: '·',
                     2: '+', 3: '-',
                     4: '*'}
-            print(dicc[matriz[i][j]], end='')
+            print(dicc[mostrable[i][j]], end='')
         if (i, columnas - 1) not in (entrada, salida):
             print('│')
         else:
@@ -80,10 +79,83 @@ def mostrar_laberinto(lab, entrada, salida):
     print('┘')
 
 
-laberinto = [[1, 1, 0, 1, 1, 1, 0, 1, 0, 0],
+def alcanzables(lab, punto, visitados):
+    filas = len(lab)
+    columnas = len(lab[0])
+    i, j = punto
+    lista = []
+    if i - 1 >= 0 and not lab[i - 1][j]:
+        lista.append((i - 1, j))
+    if i + 1 < filas and not lab[i + 1][j]:
+        lista.append((i + 1, j))
+    if j - 1 >= 0 and not lab[i][j - 1]:
+        lista.append((i, j - 1))
+    if j + 1 < columnas and not lab[i][j + 1]:
+        lista.append((i, j + 1))
+    for p in visitados:
+        if p in lista:
+            lista.remove(p)
+    return lista
+
+
+def buscar_salida(lab, entrada, salida):
+    val, err = validar_laberinto(lab, entrada, salida)
+    if not val:
+        raise ValueError(err)
+    visitar = [entrada]
+    visitados = [entrada]
+    rutas = [[entrada]]
+    while 1:
+        visitar_nuevo = []
+        if salida in visitar:
+            mostrable = laberinto_mostrable(lab, entrada, salida)
+            for r in rutas:
+                if salida in r:
+                    for p in r:
+                        mostrable[p[0]][p[1]] = 4
+            return True, mostrable
+        for nodo in visitar:
+            al = alcanzables(lab, nodo, visitados)
+            for e in al:
+                visitar_nuevo.append(e)
+                visitados.append(e)
+                for i in range(len(rutas)):
+                    for j in range(len(rutas[i])):
+                        if rutas[i][j] == nodo:
+                            rutas.append(rutas[i][:j + 1])
+                            rutas[-1].append(e)
+        visitar = visitar_nuevo
+        if len(visitar) == 0:
+            break
+    return False, []
+
+
+def correr(lab, entrada, salida):
+    print('Laberinto:')
+    mostrable = laberinto_mostrable(lab, entrada, salida)
+    mostrar_laberinto(mostrable, entrada, salida)
+    resuelve, mostrable = buscar_salida(laberinto, entrada, salida)
+    if resuelve:
+        print('Laberinto tiene solución:')
+        mostrar_laberinto(mostrable, entrada, salida)
+    else:
+        print('Laberinto NO tiene solución')
+
+
+laberinto = [[1, 1, 0, 1, 1, 1, 0, 1, 1, 1],
              [1, 1, 0, 1, 1, 0, 0, 0, 0, 0],
-             [0, 0, 0, 1, 0, 0, 1, 1, 0, 0],
-             [1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [0, 0, 0, 1, 0, 0, 1, 1, 0, 1],
+             [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+             [1, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+             [1, 1, 0, 1, 1, 1, 0, 0, 0, 0]]
+entrada, salida = (2, 0), (5, 9)
+correr(laberinto, entrada, salida)
+laberinto = [[1, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+             [1, 1, 0, 1, 1, 0, 0, 0, 0, 0],
+             [0, 0, 0, 1, 0, 0, 1, 1, 0, 1],
+             [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
              [1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
-             [0, 1, 0, 1, 1, 1, 0, 0, 0, 0]]
-mostrar_laberinto(laberinto, (2, 0), (1, 9))
+             [1, 1, 0, 1, 1, 1, 0, 0, 0, 0]]
+correr(laberinto, entrada, salida)
+salida = (1, 9)
+correr(laberinto, entrada, salida)
