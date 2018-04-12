bcaa48646250a659b7718d86fd5219fe44163c78
Marcelo Castellano <marce.geek22@gmail.com>
2018-04-12 18:30:56 -0300

    implemento clase Circulo

--- practico-02/ejercicio_02.py
+++ practico-02/ejercicio_02.py
@@ -1,13 +1,26 @@
 # Implementar la clase Circulo que contiene un radio, y sus métodos area y perimetro.
 
+import math
+
 
 class Circulo:
 
     def __init__(self, radio):
-        pass
+        if radio <= 0.0:
+            raise ValueError('radio <= 0')
+        self.radio = radio
 
     def area(self):
-        pass
+        return math.pi * self.radio ** 2
 
     def perimetro(self):
-        pass
+        return 2.0 * math.pi * self.radio
+
+
+if __name__ == '__main__':
+    circ = Circulo(2.0)
+    assert circ.perimetro() == 12.566370614359172
+    assert circ.area() == 12.566370614359172
+    circ = Circulo(4.0)
+    assert circ.perimetro() == 25.132741228718345
+    assert circ.area() == 50.26548245743669
