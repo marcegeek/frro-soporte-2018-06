f9c523732986405bc486d30a015d9f8aebe420ee
Marcelo Castellano <marce.geek22@gmail.com>
2018-04-12 18:11:38 -0300

    implemento clase Rectangulo

--- practico-02/ejercicio_01.py
+++ practico-02/ejercicio_01.py
@@ -4,7 +4,17 @@
 class Rectangulo:
 
     def __init__(self, base, altura):
-        pass
+        if base <= 0.0 or altura <= 0.0:
+            raise ValueError('base y/o altura <= 0')
+        self.base = base
+        self.altura = altura
 
     def area(self):
-        pass
+        return self.base * self.altura
+
+
+if __name__ == '__main__':
+    rect = Rectangulo(5.0, 2.0)
+    assert rect.area() == 10.0
+    rect = Rectangulo(3.0, 8.0)
+    assert rect.area() == 24.0
