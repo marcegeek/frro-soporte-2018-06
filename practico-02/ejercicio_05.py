bb5f5b48586f990833420eea52c12ef747dee73f
Marcelo Castellano <marce.geek22@gmail.com>
2018-04-19 18:45:53 -0300

    implemento función organizar_estudiantes

--- practico-02/ejercicio_05.py
+++ practico-02/ejercicio_05.py
@@ -1,8 +1,21 @@
 # Implementar la función organizar_estudiantes() que tome como parámetro una lista de Estudiantes
 # y devuelva un diccionario con las carreras como keys, y la cantidad de estudiantes en cada una de ellas como values.
 
-from practico_02.ejercicio_04 import Estudiante
+from ejercicio_04 import Estudiante
 
 
 def organizar_estudiantes(estudiantes):
-    pass
+    carreras = {}
+    for e in estudiantes:
+        if e.carrera not in carreras:
+            carreras[e.carrera] = 1
+        else:
+            carreras.update({e.carrera: carreras[e.carrera] + 1})
+    return carreras
+
+
+if __name__ == '__main__':
+    estudiantes = [Estudiante('Carlos', 23, 'M', 65.0, 1.6, 'ISI', 2010, 52, 20),
+                   Estudiante('Roberto', 21, 'M', 68.0, 1.61, 'IQ', 2010, 48, 15),
+                   Estudiante('José', 22, 'M', 70.0, 1.68, 'ISI', 2013, 52, 16)]
+    assert organizar_estudiantes(estudiantes) == {'ISI': 2, 'IQ': 1}
