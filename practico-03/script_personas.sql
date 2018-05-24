fc7fdad8faf03e6683bed5eccc40057e62c07273
Marcelo Castellano <marce.geek22@gmail.com>
2018-05-24 19:22:35 -0300

    agrego personas_persos al script de la db

--- practico-03/script_personas.sql
+++ practico-03/script_personas.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `personasdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci */;
+CREATE DATABASE  IF NOT EXISTS `personasdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `personasdb`;
--- MySQL dump 10.13  Distrib 5.7.9, for Win32 (AMD64)
+-- MySQL dump 10.16  Distrib 10.1.30-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: personasdb
+-- Host: 127.0.0.1    Database: personasdb
 -- ------------------------------------------------------
--- Server version	5.7.11-log
+-- Server version	10.1.30-MariaDB-0ubuntu0.17.10.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -34,6 +34,22 @@ CREATE TABLE `personas` (
   UNIQUE KEY `DNI_UNIQUE` (`DNI`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `personas_pesos`
+--
+
+DROP TABLE IF EXISTS `personas_pesos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personas_pesos` (
+  `IdPersona` int(11) NOT NULL,
+  `Fecha` date NOT NULL,
+  `Peso` int(11) NOT NULL,
+  PRIMARY KEY (`IdPersona`,`Fecha`),
+  CONSTRAINT `fk_personas_pesos_personas` FOREIGN KEY (`IdPersona`) REFERENCES `personas` (`IdPersona`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -44,4 +60,4 @@ CREATE TABLE `personas` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-17 18:30:03
+-- Dump completed on 2018-05-24 19:21:18
