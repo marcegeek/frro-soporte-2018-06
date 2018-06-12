b63c89820c3f1ab8bfd9ba52d37aca4f43af75e1
Marcelo Castellano <marce.geek22@gmail.com>
2018-06-12 20:42:47 -0300

    implementando alta socio

--- practico_07/presentacion_socios.py
+++ practico_07/presentacion_socios.py
@@ -36,7 +36,7 @@ class PresentacionSocios:
         self.tree.grid(sticky=tk.W + tk.E + tk.N + tk.S)
         self.frame_acciones = ttk.Frame(self.root, padding=(10, 0, 0, 10))
         self.frame_acciones.grid(row=1, column=0, sticky=tk.W + tk.E)
-        self.btn_alta = ttk.Button(self.frame_acciones, text='Alta', command=None)
+        self.btn_alta = ttk.Button(self.frame_acciones, text='Alta', command=self.alta_socio)
         self.btn_modificar = ttk.Button(self.frame_acciones, text='Modificar', command=None)
         self.btn_baja = ttk.Button(self.frame_acciones, text='Baja', command=self.baja_socio)
         self.btn_alta.pack(side=tk.LEFT, padx=(0, 5))
@@ -50,6 +50,16 @@ class PresentacionSocios:
         for s in lista_socios:
             self.tree.insert('', 'end', values=(s.id, s.nombre, s.apellido, s.dni))
 
+    def alta_socio(self):
+        ed = EditaSocio(self.root)
+        socio = ed.resultado
+        if socio:
+            try:
+                self.ns.alta(socio)
+                self.populate_treeview()
+            except Exception as err:
+                mbox.showerror('Error', str(err))
+
     def baja_socio(self):
         item_id = self.tree.focus()
         if item_id:
@@ -59,6 +69,78 @@ class PresentacionSocios:
                 id_socio = self.tree.item(item_id, 'values')[0]
                 self.ns.baja(id_socio)
                 self.populate_treeview()
+
+
+class EditaSocio:
+    def __init__(self, parent, socio=None):
+        self.parent = parent
+        self.top = tk.Toplevel(self.parent)
+        self.top.transient(self.parent)
+
+        if socio:
+            self.top.title('Editando socio')
+            self.resultado = socio
+        else:
+            self.top.title('Alta socio')
+            self.resultado = Socio(nombre='', apellido='', dni=0)
+        self.top.resizable(1, 0)
+
+        self.frame = ttk.Frame(self.top)
+        self.top.columnconfigure(0, weight=1)
+        self.frame.grid(sticky=tk.W + tk.E)
+        self.lbl_id = ttk.Label(self.frame, text='Id')
+        self.txt_id = ttk.Label(self.frame, text=str(self.resultado.id))
+        self.lbl_nombre = ttk.Label(self.frame, text='Nombre')
+        self.nombre_var = tk.StringVar()
+        self.nombre_var.set(self.resultado.nombre)
+        self.txt_nombre = ttk.Entry(self.frame, textvariable=self.nombre_var)
+        self.lbl_apellido = ttk.Label(self.frame, text='Apellido')
+        self.apellido_var = tk.StringVar()
+        self.apellido_var.set(self.resultado.apellido)
+        self.txt_apellido = ttk.Entry(self.frame, textvariable=self.apellido_var)
+        self.lbl_dni = ttk.Label(self.frame, text='DNI')
+        self.dni_var = tk.IntVar()
+        self.dni_var.set(self.resultado.dni)
+        self.txt_dni = ttk.Entry(self.frame, textvariable=self.dni_var)
+        self.frame.columnconfigure(1, weight=1)
+        self.lbl_nombre.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+        self.txt_nombre.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
+        self.lbl_apellido.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+        self.txt_apellido.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
+        self.lbl_dni.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
+        self.txt_dni.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
+
+        self.btn_frame = ttk.Frame(self.top)
+        self.btn_frame.grid(sticky=tk.E)
+        self.btn_aceptar = ttk.Button(self.btn_frame, text='Aceptar', command=self.ok)
+        self.btn_cancelar = ttk.Button(self.btn_frame, text='Cancelar', command=self.cancel)
+        self.btn_aceptar.pack(side=tk.LEFT, padx=5, pady=5)
+        self.btn_cancelar.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.top.grab_set()
+        self.top.protocol("WM_DELETE_WINDOW", self.cancel)
+        self.txt_nombre.focus_set()
+        self.top.wait_window(self.top)
+
+    def ok(self):
+        try:
+            self.resultado.dni = self.dni_var.get()
+            self.resultado.nombre = self.nombre_var.get()
+            self.resultado.apellido = self.apellido_var.get()
+            if self.resultado.nombre and self.resultado.apellido and self.resultado.dni:
+                self.finish()
+            else:
+                mbox.showinfo('Campos incompletos', 'Complete todos los campos', parent=self.top)
+        except Exception as ex:
+            mbox.showinfo('Error', str(ex), parent=self.top)
+
+    def cancel(self):
+        self.resultado = None
+        self.finish()
+
+    def finish(self):
+        self.parent.focus_set()
+        self.top.destroy()
 
 
 if __name__ == '__main__':
